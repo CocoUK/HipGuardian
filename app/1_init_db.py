bd7c830315b1b7b46dd78226fc2ed5326dd1a8fa
Coco Palafox <uk.cocopalafox@gmail.com>
2021-09-10 04:13:03 +0100

    Updated Patient model

--- app/1_init_db.py
+++ app/1_init_db.py
@@ -6,9 +6,15 @@ db.create_all()
 
 #Populate patient db
 
-entry1 = Patient(name="Alasdair", surname="Bowen", sex="M", age="25",  ethnicity = "white" )
-entry2 = Patient(name="Heather", surname="Loftus", sex="F", age="18",  ethnicity = "white" )                 
-entry3 = Patient(name="Ewan", surname="Duncan", sex="M", age="17", ethnicity = "white" )
+entry1 = Patient(name="Alasdair", surname="Bowen", sex="M", age="25",  ethnicity = "white", stage="Post-op", location ="Rehab Ward", ACS="87", mortality="8",
+                    complication="-", procedure="external fixation of fracture right angle", condition="-") 
+entry2 = Patient(name="Heather", surname="Loftus", sex="F", age="18",  ethnicity = "white" , stage="Post-op", location ="Rehab Ward", ACS="83", mortality="9",
+                    complication="Unstable blood sugars", procedure="screw and plate to left radius and ulna", condition="Type 1 Diabetes")               
+entry3 = Patient(name="Ewan", surname="Duncan", sex="M", age="17", ethnicity = "white" , stage="Post-op", location ="Rehab Ward", ACS="80", mortality="12",
+                    complication="-", procedure="screw and plate to fractured right tibia", condition="Asthma")
+
+
+ 
  
 
 db.session.add( entry1)
--- app/models.py
+++ app/models.py
@@ -17,6 +17,13 @@ class Patient(db.Model):
     admission_date = db.Column(db.DateTime, default = datetime.utcnow)
     ethnicity = db.Column(db.String(25))
     slug = db.Column(db.String(140), unique=True)
+    stage = db.Column(db.String(25))
+    location = db.Column(db.String(25))
+    ACS = db.Column(db.String(1))
+    mortality = db.Column(db.String(1))
+    procedure = db.Column(db.String(25))
+    complication = db.Column(db.String(25))
+    condition = db.Column(db.String(25))
 
 
     def __init__(self, *args, **kwargs):
@@ -30,7 +37,7 @@ class Patient(db.Model):
             self.slug = str(int(time()))
 
     def __repr__(self):
-        return f'<Patient id: {self.id}, name:{self.name}, surname:{self.surname}, age:{self.age}>'
+        return f'<Patient id: {self.id}, name:{self.name}, surname:{self.surname}, age:{self.age}, sex:{self.sex}, admission_date: {self.admission_date}, ethnicity:{self.ethnicity}, slug:{self.slug}, stage:{self.stage}, location:{self.location}, ACS:{self.ACS}, mortality:{self.mortality}, procedure:{self.procedure}, complication:{self.complication}, condition:{self.condition} >'
 
 
 
